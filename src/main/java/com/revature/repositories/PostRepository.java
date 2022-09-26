a53a378e30980189ec0d2916cd54122d90d31371
JaredMAdams <109548288+JaredMAdams@users.noreply.github.com>
2022-09-26 17:39:31 -0400

    Delete src directory

--- src/main/java/com/revature/repositories/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.revature.models.Post;
-
-public interface PostRepository extends JpaRepository<Post, Integer>{
-
-}
